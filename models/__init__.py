16c132303993d36d4a44f0af75d7a9e1644d96ec
datnguyen305 <dat3025002@gmail.com>
2025-02-03 09:33:54 +0700

    abstractiveRNN

--- models/__init__.py
+++ models/__init__.py
@@ -2,3 +2,4 @@ from .lstm.lstm import LSTM_Model
 from .bilstm.bilstm import BiLSTM_Model
 from .bigru.bigru import BiGRU_Model
 from .gru.gru import GRU_Model
+from .abstractiveRNN.abstractiveRNN import abstractiveRNN
