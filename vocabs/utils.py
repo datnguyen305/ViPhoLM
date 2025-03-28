9eacb020da19b9f73f3d73edf87cd0fe3ea015ff
Nghia Hieu Nguyen <nghiangh@uit.edu.vn>
2025-03-28 16:18:43 +0700

    added vocab for Vietnamese

--- vocabs/utils.py
+++ vocabs/utils.py
@@ -3,29 +3,10 @@ import unicodedata
 
 def preprocess_sentence(sentence: str):
     sentence = sentence.lower()
-    sentence = unicodedata.normalize("NFD", sentence)
-    sentence = re.sub(r"\s+", " ", sentence)
-    sentence = re.sub(r"!", " ! ", sentence)
-    sentence = re.sub(r"\?", " ? ", sentence)
-    sentence = re.sub(r":", " : ", sentence)
-    sentence = re.sub(r";", " ; ", sentence)
-    sentence = re.sub(r",", " , ", sentence)
-    sentence = re.sub(r"\"", " \" ", sentence)
-    sentence = re.sub(r"'", " ' ", sentence)
-    sentence = re.sub(r"\(", " ( ", sentence)
-    sentence = re.sub(r"\[", " [ ", sentence)
-    sentence = re.sub(r"\)", " ) ", sentence)
-    sentence = re.sub(r"\]", " ] ", sentence)
-    sentence = re.sub(r"/", " / ", sentence)
-    sentence = re.sub(r"\.", " . ", sentence)
-    sentence = re.sub(r"-", " - ", sentence)
-    sentence = re.sub(r"\$", " $ ", sentence)
-    sentence = re.sub(r"\&", " & ", sentence)
-    sentence = re.sub(r"\*", " * ", sentence)
-    sentence = re.sub(r"%", " % ", sentence)
-    sentence = re.sub(r"<nl>", " <nl> ", sentence) # new line mark
-
+    sentence = unicodedata.normalize("NFC", sentence)
+    # remove all non-characters and punctuations
+    sentence = re.sub(r"[-/“”!\*\&\$\.\?:;,\"'\(\[\]\(\)]", " ", sentence)
     sentence = " ".join(sentence.strip().split()) # remove duplicated spaces
     tokens = sentence.strip().split()
-
+    
     return tokens
