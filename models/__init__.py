8ca2e6e2351cc91d16f7dce1b1f0fa783297319c
datcoding <dat3052005@gmail.com>
2025-05-23 11:21:52 +0700

    adding new __init__

--- models/__init__.py
+++ models/__init__.py
@@ -3,7 +3,6 @@ from .bilstm.bilstm import BiLSTM_Model
 from .bigru.bigru import BiGRU_Model
 from .gru.gru import GRU_Model
 from .abstractiveRNN.abstractiveRNN import abstractiveRNN
-from .closedbook.closedbook import closedbook
 from .pointer_generator.model import PointerGeneratorModel
 from .transformer.model.transformer import TransformerModel
-
+from .closedbook.closedbook import Closedbook
