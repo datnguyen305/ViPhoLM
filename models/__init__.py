f61eda61e1089fbc5a371331475e76ab3d73858d
hongngoc1210 <ngochnh121@gmail.com>
2025-11-22 15:34:47 +0700

    Update seneca and  hepos

--- models/__init__.py
+++ models/__init__.py
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-from .lstm.lstm import LSTM_Model 
-from .bilstm.bilstm import BiLSTM_Model
-from .bigru.bigru import BiGRU_Model
-from .gru.gru import GRU_Model
-from .abstractiveRNN.abstractiveRNN import abstractiveRNN_model
-from .pointer_generator.model import PointerGeneratorModel
-from .transformer.model.transformer import TransformerModel
-from .closedbook.closedbook import Closedbook
-from .fast.copy_sum import CopySumm
-from .transformer_hepos.model.transformer_hepos import TransformerHeposModel
-from .transformer_seal.model.transformer_seal import TransformerSealModel
-from .seal_closedbook.seal_closedbook import ClosedbookSeal
-from .fast_seal.copy_sum import CopySummSeal
-from .hepos.hepos import HEPOSBaselineSummarizer
-from .hat.hat_model import HATModel
-from .seneca.seneca import SENECAModel
-from .longformer.longformer_encoder_decoder import LongformerEncoderDecoderModel
-=======
 from .lstm.lstm import LSTM_Model 
 from .bilstm.bilstm import BiLSTM_Model
 from .bigru.bigru import BiGRU_Model
@@ -34,4 +15,3 @@ from .hat.hat_model import HATModel
 from .seneca.seneca import SENECAModel
 from .longformer.longformer_encoder_decoder import LongformerEncoderDecoderModel
 from .bottom_up.model import BottomUpSummarizer
->>>>>>> 79161b76d3dc1e448a317bcf1d79d76cd5ccd160
