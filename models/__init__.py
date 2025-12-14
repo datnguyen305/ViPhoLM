208750f0de872590b21033ab8dcf54b365d10671
datcoding <dat3052005@gmail.com>
2025-12-14 16:25:55 +0700

    delete transformer hepos, seal, temporary

--- models/__init__.py
+++ models/__init__.py
@@ -6,8 +6,8 @@ from .AbstractiveTXModel.AbstractiveTXModel import AbstractiveTXModel
 from .pointer_generator.pointer_generator import PointerGenerator
 from .transformer.transformer import TransformerModel
 from .closedbook.closedbook import ClosedBookModel
-from .transformer_hepos.model.transformer_hepos import TransformerHeposModel
-from .transformer_seal.model.transformer_seal import TransformerSealModel
+# from .transformer_hepos.model.transformer_hepos import TransformerHeposModel
+# from .transformer_seal.model.transformer_seal import TransformerSealModel
 from .seal_closedbook.seal_closedbook import ClosedbookSeal
 from .fast_seal.copy_sum import CopySummSeal
 # from .hepos.hepos import HeposFairseqBaseline
