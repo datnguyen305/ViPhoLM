d49d47db694cf5662a7bd158e264766eb9c9f77c
hongngoc1210 <ngochnh121@gmail.com>
2026-01-09 23:59:14 +0700

    update seneca changes

--- data_utils/__init__.py
+++ data_utils/__init__.py
@@ -4,9 +4,10 @@ from utils.instance import Instance, InstanceList
 from .text_sum_dataset import TextSumDataset
 from .text_sum_dataset_hierarchical import TextSumDatasetHierarchical   
 from .text_sum_dataset_oov import TextSumDatasetOOV
-from .text_sum_dataset_phoneme import TextSumDatasetPhoneme
+from .text_sum_dataset_seneca import TextSumDatasetSeneca
 import torch
 from torch.nn.utils.rnn import pad_sequence
+import torch.nn.functional as F
 
 def collate_fn(items: List[Instance]) -> InstanceList:
     return InstanceList(items)
@@ -70,3 +71,63 @@ def collate_fn_oov(items: List[Instance]) -> Instance:
         extended_source_idx = input_ids_padded,
         extra_zeros = extra_zeros
     )
+def collate_fn_seneca(batch):
+    """
+    batch: List[Instance]
+    """
+
+    pad_idx = batch[0].vocab.pad_idx if hasattr(batch[0], "vocab") else 0
+
+    # -------- sentences --------
+    batch_sentences = []
+    max_sent = max(len(x.sentences) for x in batch)
+    max_len = max(
+        max(len(s) for s in x.sentences) for x in batch
+    )
+
+    for item in batch:
+        sent_tensors = []
+        for s in item.sentences:
+            if isinstance(s, torch.Tensor):
+                s = s.clone().detach()
+            else:
+                s = torch.tensor(s, dtype=torch.long)
+            if len(s) < max_len:
+                s = F.pad(s, (0, max_len - len(s)), value=pad_idx)
+            sent_tensors.append(s)
+
+        # pad number of sentences
+        if len(sent_tensors) < max_sent:
+            pad_sent = torch.full(
+                (max_len,), pad_idx, dtype=torch.long
+            )
+            sent_tensors.extend(
+                [pad_sent] * (max_sent - len(sent_tensors))
+            )
+
+        batch_sentences.append(torch.stack(sent_tensors))
+
+    sentences = torch.stack(batch_sentences)  # (B, N, L)
+
+    # -------- entities (keep as list) --------
+    entities = [x.entities for x in batch]
+
+    # -------- target --------
+    labels = [
+        x.label.clone().detach()
+        if isinstance(x.label, torch.Tensor)
+        else torch.tensor(x.label, dtype=torch.long)
+        for x in batch
+    ]
+
+    labels = torch.nn.utils.rnn.pad_sequence(
+        labels, batch_first=True, padding_value=pad_idx
+    )
+
+    return {
+        "sentences": sentences,
+        "entities": entities,
+        "label": labels,
+        "id": [x.id for x in batch]
+    }
+
