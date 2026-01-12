92c16bbde80472b8e57f675d65e693470bc5d317
datcoding <dat3052005@gmail.com>
2026-01-12 16:47:07 +0700

    fix: vocabs/utils.py

--- vocabs/utils.py
+++ vocabs/utils.py
@@ -3,10 +3,29 @@ import unicodedata
 
 def preprocess_sentence(sentence: str):
     sentence = sentence.lower()
-    sentence = unicodedata.normalize("NFC", sentence)
-    # remove all non-characters and punctuations
-    sentence = re.sub(r"[-/“”!\*\&\$\.\?:;,\"'\(\[\]\(\)]", " ", sentence)
+    sentence = unicodedata.normalize("NFD", sentence)
+    sentence = re.sub(r"\s+", " ", sentence)
+    sentence = re.sub(r"!", " ! ", sentence)
+    sentence = re.sub(r"\?", " ? ", sentence)
+    sentence = re.sub(r":", " : ", sentence)
+    sentence = re.sub(r";", " ; ", sentence)
+    sentence = re.sub(r",", " , ", sentence)
+    sentence = re.sub(r"\"", " \" ", sentence)
+    sentence = re.sub(r"'", " ' ", sentence)
+    sentence = re.sub(r"\(", " ( ", sentence)
+    sentence = re.sub(r"\[", " [ ", sentence)
+    sentence = re.sub(r"\)", " ) ", sentence)
+    sentence = re.sub(r"\]", " ] ", sentence)
+    sentence = re.sub(r"/", " / ", sentence)
+    sentence = re.sub(r"\.", " . ", sentence)
+    sentence = re.sub(r"-", " - ", sentence)
+    sentence = re.sub(r"\$", " $ ", sentence)
+    sentence = re.sub(r"\&", " & ", sentence)
+    sentence = re.sub(r"\*", " * ", sentence)
+    sentence = re.sub(r"%", " % ", sentence)
+    sentence = re.sub(r"<nl>", " <nl> ", sentence) # new line mark
+
     sentence = " ".join(sentence.strip().split()) # remove duplicated spaces
     tokens = sentence.strip().split()
-    
+
     return tokens
