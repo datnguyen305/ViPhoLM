80a15bf40568b32b136754988a8477042f8eec87
datcoding <dat3052005@gmail.com>
2026-03-15 11:08:17 +0700

    fixing padding for longformer

--- models/transformer_phoneme_longformer/utils/padding_mask.py
+++ models/transformer_phoneme_longformer/utils/padding_mask.py
@@ -1,7 +1,21 @@
+import torch
+
 def create_padding_mask(seq, pad_idx):
     """
-    Tạo mask cho key_padding_mask (True là Pad).
-    Shape: (Batch_Size, Seq_Len, 3)
+    Tạo attention_mask chuẩn cho Longformer.
+    Shape: (Batch_Size, Seq_Len)
+    Giá trị:
+      -1: No attention (Dành cho Padding)
+       0: Local attention (Cửa sổ trượt Sliding window)
+       1: Global attention (Dành cho token đặc biệt gom ngữ cảnh)
     """
     seq = seq[:, :, 0]
-    return (seq == pad_idx)
+    
+    mask = torch.zeros_like(seq, dtype=torch.long)
+    
+    is_pad = (seq == pad_idx)
+    mask[is_pad] = -1
+    
+    mask[:, 0] = 1
+    
+    return mask
